46a6c4f2d0f032c8dfe2325c4a82d0436d53da42
Carlos <luissalasjh13@gmail.com>
2024-09-11 21:45:18 -0700

    Body y footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router'; 
 import { InicioComponent } from './inicio/inicio.component'; 
+import { FooterComponent } from './footer/footer.component';
+import { BodyComponent } from './body/body.component';
 
 // Define las rutas que necesitas aquí
 export const routes: Routes = [
 
-
- 
 ];
 
 @NgModule({
