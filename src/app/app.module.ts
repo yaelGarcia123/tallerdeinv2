2c78bc7e507884852db2a0f25912507ac050ff08
Carlos <luissalasjh13@gmail.com>
2024-09-11 22:06:58 -0700

    pantalla inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,29 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 // Importa tus módulos de rutas
-
 import { AppRoutingModule } from './app.routes'; // Asegúrate de que la ruta y nombre sean correctos
 import { AppComponent } from './app.component'; // Asegúrate de tener este componente creado
 import { InicioComponent } from './inicio/inicio.component';
-<<<<<<< HEAD
 import { LoginComponent } from './login/login.component';
-
-=======
 import { FooterComponent } from './footer/footer.component';
 import { BodyComponent } from './body/body.component';
->>>>>>> carlossalas
+
 
 @NgModule({
   declarations: [
     AppComponent,
     InicioComponent,
-<<<<<<< HEAD
-    LoginComponent
-=======
+    LoginComponent,
     BodyComponent,
     FooterComponent
->>>>>>> carlossalas
+
   ],
+
   imports: [
     BrowserModule,
     FormsModule,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,27 +1,21 @@
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router'; 
 import { InicioComponent } from './inicio/inicio.component'; 
-<<<<<<< HEAD
 import { LoginComponent } from './login/login.component';
+import { FooterComponent } from './footer/footer.component';
+import { BodyComponent } from './body/body.component';
 export const routes: Routes = [
 
 { path: '', redirectTo: 'inicio', pathMatch: 'full' }, // Redirecciona la ruta raíz a 'inicio' (opcional)
 
   { path: 'login', component: LoginComponent },
 
-  { path: 'inicio', component: InicioComponent }, // Define la ruta
+  { path: 'inicio', component: InicioComponent } // Define la ruta
   
 
 
  
-=======
-import { FooterComponent } from './footer/footer.component';
-import { BodyComponent } from './body/body.component';
-
-// Define las rutas que necesitas aquí
-export const routes: Routes = [
-
->>>>>>> carlossalas
 ];
 
 @NgModule({
