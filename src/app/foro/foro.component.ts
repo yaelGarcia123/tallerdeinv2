074a296eadb82e7dec0dc0d8455e1db0abc490c0
Carlos <luissalasjh13@gmail.com>
2024-11-05 23:28:16 -0700

    barrabusqueda

--- src/app/foro/foro.component.ts
+++ src/app/foro/foro.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,29 +9,43 @@ import { Component } from '@angular/core';
 export class ForoComponent {
   isSidebarVisible: boolean = true; // Estado del menú lateral (visible/invisible)
   currentSection: string = 'home'; // Sección actual por defecto
+  searchQuery: string = ''; // Consulta de búsqueda
+  
+  constructor(private router: Router) {}
+
 
   posts = [
     {
-      usuario: 'Usuario1',
+      usuario: 'Carlos Salas - Ingenieros en informatica, Mexico',
       contenido: 'Esta es una publicación interesante sobre la naturaleza.',
-      imagen: 'https://via.placeholder.com/600x300?text=Imagen+de+Naturaleza',
+      imagen: 'assets/img/ingenieriasoftware.jpg',
       fecha: 'Hace 2 horas'
     },
     {
-      usuario: 'Usuario2',
+      usuario: 'Yael Garcia - Ingenieros de software, Mexico',
       contenido: 'Compartiendo una actualización sobre mi día. ¡Espero que les guste!',
-      imagen: '',
+      imagen: 'assets/img/ingenieriasoftware.jpg',
       fecha: 'Hace 5 horas'
     },
     {
-      usuario: 'Usuario3',
+      usuario: 'Kevin chavez - Ingenieros Mecanicos, Mexico',
       contenido: 'Aquí les dejo una foto de mi reciente viaje.',
-      imagen: 'https://via.placeholder.com/600x300?text=Viaje+Increíble',
+      imagen: 'assets/img/ingenieriamecanicaa.jpg',
       fecha: 'Ayer'
     }
   ];
 
-  friends = ['Amigo1', 'Amigo2', 'Amigo3', 'Amigo4'];
+
+  // Lista de grupos de ejemplo
+  groups = [
+    { imagen: 'assets/img/ingenieriainformatica.jpg', nombre: 'Ingenieros en Informática', descripcion: 'Grupo sobre informática y software.', miembros: 120 },
+    { imagen: 'assets/img/ingenieriamecanicaa.jpg', nombre: 'Ingenieros Mecánicos', descripcion: 'Grupo de ingenieros mecánicos en México.', miembros: 85 },
+    { imagen: 'assets/img/ingenieriasoftware.jpg', nombre: 'Ingeniería Software', descripcion: 'Discusiones sobre medio ambiente y sostenibilidad.', miembros: 64 }
+    // Agrega más grupos según sea necesario
+  ];
+
+  // Grupos filtrados para mostrar según la búsqueda
+  filteredGroups = [...this.groups];
 
   // Método para alternar la visibilidad del menú lateral
   toggleSidebar() {
@@ -46,5 +61,14 @@ export class ForoComponent {
   logout() {
     console.log('Cerrar sesión');
     // Aquí puedes agregar lógica adicional para manejar el cierre de sesión
+    this.router.navigate(['/foro']);
+  }
+
+  // Método para filtrar grupos según la búsqueda
+  filterGroups() {
+    this.filteredGroups = this.groups.filter(group => 
+      group.nombre.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
+      group.descripcion.toLowerCase().includes(this.searchQuery.toLowerCase())
+    );
   }
 }
